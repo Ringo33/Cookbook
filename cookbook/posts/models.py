2621f3d7b3c4fb0631b008cce0afae927fb8d04c
Rinat <masagutov33@gmail.com>
2022-08-10 18:15:02 +0300

    commit

---
+++ cookbook/posts/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Category(models.Model):
+    title = models.CharField(max_length=40, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    upd_date = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name="posts"
+    )
+    image = models.ImageField(
+        upload_to='photos/%Y/%m/%d/',
+        blank=True,
+        null=True,
+        verbose_name='Фото'
+    )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+        ordering = ['-pub_date']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(
+        max_length=1000,
+        verbose_name='Комментарий к посту'
+    )
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата комментария')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created']
