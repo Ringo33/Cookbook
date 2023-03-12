ec73667a33242b6aa1415464043fc5f6ea28c495
Rinat <masagutov33@gmail.com>
2023-03-12 21:59:09 +0300

    new one

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -100,21 +100,32 @@ WSGI_APPLICATION = 'cookbook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'new2',
-#         'USER': 'root',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'django_db',
+#         'USER': 'django_user',
 #         'PASSWORD': 'Bingo555',
-#         'HOST': 'localhost',
-#         # 'PORT': '3306',
+#         'HOST': 'db',
+#         'PORT': '5432',
 #     }
 # }
 
@@ -128,6 +139,7 @@ DATABASES = {
 #         'PORT': '3306',
 #     }
 # }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
